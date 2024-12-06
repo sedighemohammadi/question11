dbf0d623ebb085cb51940e824733319cfe2e2085
sedighemohammadi <sedighemohammadi387@gmail.com>
2024-12-06 20:15:29 +0330

    6

---
+++ q6.js
@@ -0,0 +1,18 @@
+// ? 6. We have an object called 'user'.
+// Write the destructuring assignment that converts:
+// todo1 : 'name' property into the variable 'firstName'.
+// todo2 : 'years' property into the variable 'age'.
+// todo3 : 'admin' property into the variable 'isAdmin'.(if no such property exists create 'isAdmin' variable and assign false value to that)
+
+let user = { name: 'John', years: 30 };
+
+// console.log(name); // John
+// console.log(age); // 30
+// console.log(isAdmin); // false
+
+// ! Answer
+let {name : fisrtName , years : age , admin : isAdmin = false } = user
+console.log(fisrtName)
+console.log(age)
+console.log(isAdmin)
+
