46fb960f11c1a8fd9cfe2663b7942e759caa6dd0
sedighemohammadi <sedighemohammadi387@gmail.com>
2024-12-06 20:15:17 +0330

    4

---
+++ q4.js
@@ -0,0 +1,16 @@
+// ? 4. Change two or more values of this object .
+// todo : one of the values that you change must be the skills property // example: ['python', 'css', 'js']
+
+const obj2 = {
+    id: 'kd455',
+    age: 24,
+    username: 'john2000',
+    skills: ['html', 'css', 'js'],
+  };
+  
+  //  !   Answer:
+  obj2.skills = ['html' , 'python' , 'js']
+  obj2.age = 20 
+  obj2.id = 'sedigh874'
+  
+  console.log(obj2)
