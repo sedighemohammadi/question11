07e53f8e81119e26124b5ed32cd649c7d413d098
sedighemohammadi <sedighemohammadi387@gmail.com>
2024-12-06 20:15:37 +0330

    7

---
+++ q7.js
@@ -0,0 +1,10 @@
+// ? 7. Using Array Destructuring, get the last name from the array.
+//  todo : notice that 'Christina' is firstName , 'Jon' is middleName , 'Alexandare' is lastName.
+
+const students = ['Christina', 'Jon', 'Alexandare'];
+const [firstName , middleName , lastName] = students
+console.log(firstName)
+console.log(middleName)
+console.log(lastName)
+
+// ! Answer
